8ac6f2de43314b6a403bb49dfb44de52868fe38f
gitme <git@gmail.com>
2022-02-04 19:38:28 +0800

    restore http3

---
+++ http3_conn.go
@@ -0,0 +1,250 @@
+package core
+
+import (
+	"crypto/tls"
+	"encoding/binary"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/polevpn/h3conn"
+)
+
+const (
+	CH_HTTP3_WRITE_SIZE     = 100
+	HTTP3_HANDSHAKE_TIMEOUT = 5
+)
+
+type Http3Conn struct {
+	up      uint64
+	down    uint64
+	conn    *h3conn.Conn
+	wch     chan []byte
+	closed  bool
+	handler map[uint16]func(PolePacket, Conn)
+	mutex   *sync.RWMutex
+}
+
+func NewHttp3Conn() *Http3Conn {
+	return &Http3Conn{
+		conn:    nil,
+		closed:  true,
+		wch:     nil,
+		handler: make(map[uint16]func(PolePacket, Conn)),
+		mutex:   &sync.RWMutex{},
+	}
+}
+
+func (h3c *Http3Conn) Connect(endpoint string, user string, pwd string, ip string, sni string, skipVerifySSL bool, header http.Header) error {
+
+	var err error
+
+	tlsconfig := &tls.Config{
+		InsecureSkipVerify: skipVerifySSL,
+		ServerName:         sni,
+	}
+
+	client := h3conn.NewClient(tlsconfig)
+
+	conn, resp, err := client.Connect(endpoint+"?user="+url.QueryEscape(user)+"&pwd="+url.QueryEscape(pwd)+"&ip="+ip, time.Second*HTTP3_HANDSHAKE_TIMEOUT, header)
+
+	if err != nil {
+		if resp != nil {
+			if resp.StatusCode == http.StatusBadRequest {
+				resp.Body.Close()
+				return ErrIPNotExist
+			} else if resp.StatusCode == http.StatusForbidden {
+				resp.Body.Close()
+				return ErrLoginVerify
+			} else {
+				resp.Body.Close()
+				return ErrConnectUnknown
+			}
+		}
+		plog.Error("http3 connect fail,", err)
+		return ErrNetwork
+	}
+
+	h3c.mutex.Lock()
+	defer h3c.mutex.Unlock()
+
+	h3c.conn = conn
+	h3c.wch = make(chan []byte, CH_HTTP3_WRITE_SIZE)
+	h3c.closed = false
+	return nil
+}
+
+func (h3c *Http3Conn) Close(flag bool) error {
+	h3c.mutex.Lock()
+	defer h3c.mutex.Unlock()
+
+	if !h3c.closed {
+		h3c.closed = true
+		if h3c.wch != nil {
+			close(h3c.wch)
+		}
+
+		err := h3c.conn.Close()
+		if flag {
+			pkt := make([]byte, POLE_PACKET_HEADER_LEN)
+			PolePacket(pkt).SetCmd(CMD_CLIENT_CLOSED)
+			go h3c.dispatch(pkt)
+		}
+		return err
+	}
+	return nil
+}
+
+func (h3c *Http3Conn) String() string {
+	return h3c.conn.LocalAddr().String() + "->" + h3c.conn.RemoteAddr().String()
+}
+
+func (h3c *Http3Conn) IsClosed() bool {
+
+	return h3c.closed
+}
+
+func (h3c *Http3Conn) SetHandler(cmd uint16, handler func(PolePacket, Conn)) {
+	h3c.handler[cmd] = handler
+}
+
+func (h3c *Http3Conn) read() {
+	defer func() {
+		h3c.Close(true)
+	}()
+
+	defer PanicHandler()
+
+	for {
+		var preOffset = 0
+
+		prefetch := make([]byte, 2)
+
+		for {
+			n, err := h3c.conn.Read(prefetch[preOffset:])
+			if err != nil {
+				if err == io.EOF || strings.Contains(err.Error(), "canceled") {
+					plog.Info(h3c.String(), " conn closed")
+				} else {
+					plog.Error(h3c.String(), " conn read exception:", err)
+				}
+				return
+			}
+			preOffset += n
+			if preOffset >= 2 {
+				break
+			}
+		}
+
+		length := binary.BigEndian.Uint16(prefetch)
+
+		if length < POLE_PACKET_HEADER_LEN {
+			plog.Error("invalid packet len")
+			continue
+		}
+
+		pkt := make([]byte, length)
+		copy(pkt, prefetch)
+		var offset uint16 = 2
+		for {
+			n, err := h3c.conn.Read(pkt[offset:])
+			if err != nil {
+				if err == io.EOF || strings.Contains(err.Error(), "canceled") {
+					plog.Info(h3c.String(), " conn closed")
+				} else {
+					plog.Error(h3c.String(), " conn read exception:", err)
+				}
+				return
+			}
+			offset += uint16(n)
+			if offset >= length {
+				break
+			}
+		}
+		atomic.AddUint64(&h3c.down, uint64(len(pkt)))
+		h3c.dispatch(pkt)
+
+	}
+
+}
+
+func (h3c *Http3Conn) dispatch(pkt []byte) {
+	ppkt := PolePacket(pkt)
+
+	handler, ok := h3c.handler[ppkt.Cmd()]
+	if ok {
+		handler(pkt, h3c)
+	} else {
+		plog.Error("invalid pkt cmd=", ppkt.Cmd())
+	}
+}
+
+func (h3c *Http3Conn) drainWriteCh() {
+	for {
+		select {
+		case _, ok := <-h3c.wch:
+			if !ok {
+				return
+			}
+		default:
+			return
+		}
+	}
+}
+
+func (h3c *Http3Conn) write() {
+	defer PanicHandler()
+	defer h3c.drainWriteCh()
+
+	for {
+		select {
+		case pkt, ok := <-h3c.wch:
+			if !ok {
+				plog.Info("http3 conn writing channel closed")
+				return
+			} else {
+				if pkt == nil {
+					plog.Info("exit write process")
+					return
+				}
+				atomic.AddUint64(&h3c.up, uint64(len(pkt)))
+				_, err := h3c.conn.Write(pkt)
+				if err != nil {
+					if err == io.EOF || err == io.ErrUnexpectedEOF {
+						plog.Info(h3c.String(), " conn closed")
+					} else {
+						plog.Error(h3c.String(), " conn write exception:", err)
+					}
+					return
+				}
+			}
+		}
+	}
+}
+
+func (h3c *Http3Conn) GetUpDownBytes() (uint64, uint64) {
+	return h3c.up, h3c.down
+}
+
+func (h3c *Http3Conn) Send(pkt []byte) {
+
+	h3c.mutex.RLock()
+	defer h3c.mutex.RUnlock()
+
+	if h3c.IsClosed() {
+		plog.Debug("websocket connection is closed,can't send pkt")
+		return
+	}
+	if h3c.wch != nil {
+		h3c.wch <- pkt
+	}
+}
+
+func (h3c *Http3Conn) StartProcess() {
+	go h3c.read()
+	go h3c.write()
+}
